3a2a2595f230759892a07a7f9e5268743b1ff675
Johnwjackson@yahoo.comgit config --global user.name JohnJgit config --global user.email Johnwjackson@yahoo.com <Johnwjackson@yahoo.com>
2022-01-10 17:50:36 -0500

    Added playRound function that takes a player selection and the computer selection and returns an array of the winner and a string congraulatory message

--- game.js
+++ game.js
@@ -13,4 +13,34 @@ function computerPlay() {
   }
 }
 
+function playRound(playerSelection, cpuSelection) {
+  const lowerPlayer = playerSelection.toLowerCase();
+  const cpuPlayer = cpuSelection.toLowerCase();
 
+  if (lowerPlayer == cpuPlayer) {
+    return [0, "It's a tie!"];
+  }
+  else if (lowerPlayer == "scissors") {
+    if (cpuPlayer == "rock") {
+      return [1, "You lose. Rock beats scissors."]
+    }
+    if (cpuPlayer == "paper") {
+      return [2, "You win! Scissors beat paper."];
+    }
+  }
+  else if (lowerPlayer == "paper") {
+    if (cpuPlayer == "rock") {
+      return [2, "You win! Paper beats rock."];
+    }
+    if (cpuPlayer == "scissors") {
+      return [1, "You lose. Scissors beats paper."];
+    }
+  }
+  else {
+    if (cpuPlayer == "paper") {
+      return [1, "You lose. Paper beats rock."];
+    }
+    return [2, "You win! Rock beats scissors."];
+
+  }
+}
