b80cb54d43e01cd9c1747080e1e86ab6a3a2ce7b
Johnwjackson@yahoo.comgit config --global user.name JohnJgit config --global user.email Johnwjackson@yahoo.com <Johnwjackson@yahoo.com>
2022-01-10 17:49:29 -0500

    Added computerPlay() function that returns rock, paper or scissors at random

---
+++ game.js
@@ -0,0 +1,16 @@
+function computerPlay() {
+  let random = Math.floor(Math.random() * (3 - 1 + 1) + 1);
+  switch (random) {
+    case 1: 
+      return "Rock";
+      break;
+    case 2:
+      return "Paper";
+      break;
+    case 3:
+      return "Scissors";
+      break;
+  }
+}
+
+
